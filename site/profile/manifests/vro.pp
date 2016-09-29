e6f3aface1ec85ff19084ad282f7521b4387dbc8
jpadams <jeremy@puppetlabs.com>
2016-09-29 15:26:29 -0700

    Update vro.pp

--- site/profile/manifests/vro.pp
+++ site/profile/manifests/vro.pp
@@ -1,4 +1,14 @@
 #
 class profile::vro {
 
+  case $kernel {
+    'Linux': {
+      file { '/etc/puppetlabs/puppet/csr_attributes.yaml':
+        ensure => absent,
+      }
+    }
+    'windows': { }
+    default: {}
+  }
+
 }
