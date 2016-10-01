8ae2f65644699b3c380cca947fe8ec506dcce10f
jpadams <jeremy@puppetlabs.com>
2016-10-01 08:21:10 -0700

    Update vro.pp

--- site/profile/manifests/vro.pp
+++ site/profile/manifests/vro.pp
@@ -8,12 +8,18 @@ class profile::vro_provisioned {
     backup => false,
   }
 
-# any operating system-specific state for vro-provisioned machines.
-# it's common to change the Administrator user name on Windows, for example.
-case $kernel {
-  'Linux': {}
-  'windows': {}
-  default: {}
-}
+  # any operating system-specific state for vro-provisioned machines.
+  # it's common to change the Administrator user name on Windows, for example.
+  case $kernel {
+    'Linux': {}
+    'windows': {
+      exec { 'rename-admin':
+        command  => '$(Get-WMIObject Win32_UserAccount -Filter "Name=\'Administrator\'").Rename("puppet#adm1n")',
+        unless   => 'if (Get-WmiObject Win32_UserAccount -Filter "Name=\'Administrator\'") { exit 1 }',
+        provider => powershell,
+      }
+    }
+    default: {}
+  }
 
 }
