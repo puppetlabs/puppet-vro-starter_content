d3d2dd3ad7a55d065761503fba973881b6f12e62
jpadams <jeremy@puppetlabs.com>
2016-10-01 08:10:30 -0700

    Update vro.pp

--- site/profile/manifests/vro.pp
+++ site/profile/manifests/vro.pp
@@ -1,14 +1,19 @@
 #
-class profile::vro {
+class profile::vro_provisioned {
 
-  case $kernel {
-    'Linux': {
-      file { '/etc/puppetlabs/puppet/csr_attributes.yaml':
-        ensure => absent,
-      }
-    }
-    'windows': { }
-    default: {}
+  # ensure that the csr_attributes.yaml file containing the shared
+  # autosign secret is removed on the first puppet agent run.
+  file { "${settings::confdir}/csr_attributes.yaml":
+    ensure => absent,
+    backup => false,
   }
+
+# any operating system-specific state for vro-provisioned machines.
+# it's common to change the Administrator user name on Windows, for example.
+case $kernel {
+  'Linux': {}
+  'windows': {}
+  default: {}
+}
 
 }
