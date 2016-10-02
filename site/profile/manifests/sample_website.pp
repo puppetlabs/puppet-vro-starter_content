0092324cefda81839783246a3f609e7725a7dc30
jpadams <jeremy@puppetlabs.com>
2016-10-02 13:49:09 -0700

    Update sample_website.pp

--- site/profile/manifests/sample_website.pp
+++ site/profile/manifests/sample_website.pp
@@ -3,18 +3,28 @@ class profile::sample_website {
 
   case $::kernel {
     'windows': {
-      staging::deploy { 'sample_website.zip':
+#      staging::deploy { 'sample_website.zip':
+#        source  => 'puppet:///modules/profile/sample_website.zip',
+#        target  => 'C:\inetpub\wwwroot\sample_website',
+#        require => [ Iis::Manage_site[$::fqdn], Package['unzip'] ],
+#        creates => 'C:\inetpub\wwwroot\sample_website\index.html',
+#      }
+      file { 'C:\sample_website.zip':
+        ensure => file,
         source  => 'puppet:///modules/profile/sample_website.zip',
-        target  => 'C:\inetpub\wwwroot\sample_website',
-        require => Iis::Manage_site[$::fqdn],
-        creates => 'C:\inetpub\wwwroot\sample_website\index.html',
+      }
+
+      unzip { 'sample_website.zip':
+        source      => 'C:\sample_website.zip',
+        destination => 'C:\inetpub\wwwroot\sample_website',
+        creates     => 'C:\inetpub\wwwroot\sample_website\index.html',
       }
     }
     'linux':   {
       staging::deploy { 'sample_website.zip':
         source  => 'puppet:///modules/profile/sample_website.zip',
         target  => '/var/www/sample_website',
-        require => Apache::Vhost[$::fqdn],
+        require => [ Apache::Vhost[$::fqdn], Package['unzip'] ],
         creates => '/var/www/sample_website/index.html',
       }
     }
