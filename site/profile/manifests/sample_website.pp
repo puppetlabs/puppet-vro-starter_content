877a7705b6190af51a55905a65a1d5c663fe0509
jpadams <jeremy@puppetlabs.com>
2016-10-02 21:51:34 -0700

    Update sample_website.pp

--- site/profile/manifests/sample_website.pp
+++ site/profile/manifests/sample_website.pp
@@ -5,6 +5,7 @@ class profile::sample_website {
     'windows': {
       require profile::iis
       require profile::windows_baseline
+      
       staging::deploy { 'sample_website.zip':
         source  => 'puppet:///modules/profile/sample_website.zip',
         target  => 'C:\inetpub\wwwroot\sample_website',
@@ -14,6 +15,7 @@ class profile::sample_website {
     'linux':   {
       require profile::apache
       require profile::linux_baseline
+      
       staging::deploy { 'sample_website.zip':
         source  => 'puppet:///modules/profile/sample_website.zip',
         target  => '/var/www/sample_website',
