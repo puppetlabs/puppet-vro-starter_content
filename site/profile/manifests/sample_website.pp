ee9ac7cf6b890c0d8ba363ed28cc5b60a92e672e
jpadams <jeremy@puppetlabs.com>
2016-10-02 21:40:50 -0700

    Update sample_website.pp

--- site/profile/manifests/sample_website.pp
+++ site/profile/manifests/sample_website.pp
@@ -3,26 +3,18 @@ class profile::sample_website {
 
   case $::kernel {
     'windows': {
-      $staging_dir = 'C:\staging'
-
-      file { $staging_dir:
-        ensure => directory,
-      }
-      file { "${staging_dir}\\sample_website.zip":
-        ensure => file,
+      require profile::iis
+      staging::deploy { 'sample_website.zip':
         source  => 'puppet:///modules/profile/sample_website.zip',
-      }
-      unzip { 'sample_website.zip':
-        source      => "${staging_dir}\\sample_website.zip",
-        destination => 'C:\inetpub\wwwroot\sample_website',
-        creates     => 'C:\inetpub\wwwroot\sample_website\index.html',
+        target  => 'C:\inetpub\wwwroot\sample_website',
+        creates => 'C:\inetpub\wwwroot\sample_website\index.html',
       }
     }
     'linux':   {
+      require profile::apache
       staging::deploy { 'sample_website.zip':
         source  => 'puppet:///modules/profile/sample_website.zip',
         target  => '/var/www/sample_website',
-        require => Apache::Vhost[$::fqdn],
         creates => '/var/www/sample_website/index.html',
       }
     }
