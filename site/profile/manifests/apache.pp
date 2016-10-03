a438586947821f1d3d3f3f93f34b9894f13f93aa
jpadams <jeremy@puppetlabs.com>
2016-10-02 18:59:07 -0700

    Update apache.pp

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -20,12 +20,11 @@ class profile::apache {
     require => File[$doc_root],
   }
 
-# Uncomment the following code to configure firewalld
-#  firewalld_port { 'Open port 80 for web':
-#    ensure   => present,
-#    zone     => 'public',
-#    port     => 80,
-#    protocol => 'tcp',
-#  }
+  firewalld_port { 'Open port 80 for web':
+    ensure   => present,
+    zone     => 'public',
+    port     => 80,
+    protocol => 'tcp',
+  }
 
 }
