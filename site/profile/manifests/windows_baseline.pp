85ed3b8c20dc851dc0abe39f1d9914333b6e993f
jpadams <jeremy@puppetlabs.com>
2016-10-02 21:46:28 -0700

    Update windows_baseline.pp

--- site/profile/manifests/windows_baseline.pp
+++ site/profile/manifests/windows_baseline.pp
@@ -2,7 +2,12 @@
 class profile::windows_baseline {
 
   include chocolatey
-
+  
+  package { 'unzip':
+    ensure   => installed,
+    provider => chocolatey,
+  }
+      
   package { 'git':
     ensure   => installed,
     provider => chocolatey,
