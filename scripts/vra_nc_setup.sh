ce6d4f08758c95eb7dc0dc030b83f07618788df7
maju6406 <beersy@gmail.com>
2016-11-30 13:06:36 -0800

    Adding more error checks

--- scripts/vra_nc_setup.sh
+++ scripts/vra_nc_setup.sh
@@ -2,7 +2,7 @@
 # This script automates the NC classification and environment group setup for the VRO plugin provisioning workflow
 # Run this as root on your master
 # Note: this script does not randomize uuid for the classification group it creates, so it will create/replace the same group everytime instead of creating a new group
-#
+# This script assumes it is being run on a freshly installed master that is not using code manager.
 #
 # User configuration
 #
@@ -30,11 +30,33 @@ find_guid()
 }
 echo Puppet Master Setup Script
 echo --------------------------
-echo This script expects to be run from puppet-vro-starter_content directory. If run from a different directory, the script may fail.
+echo This script expects to be run from puppet-vro-starter_content directory. If run from a different directory, the script will fail.
+echo This script also assumes it is being run on a freshly installed master that is not using code manager.
+echo --------------------------
+
+#
+# Check if code manager is being used
+#
+curl -s -X GET \ -H "Content-Type: application/json" \
+--cert   $cert \
+--key    $key \
+--cacert $cacert \
+"https://$master_hostname:4433/classifier-api/v1/groups" | python -m json.tool | grep -q code_manager_auto_configure
+if [ $? -eq 0 ]; then
+  echo "ERROR: It appears that code manager is being used. This script can not continue."
+  exit 1
+fi
+
+if [ -d /etc/puppetlabs/code/environments/$alternate_environment ]; then
+  echo "ERROR: It appears that the \"$alternate_environment\" environment already exists. Please remove this /etc/puppetlabs/code/environments/$alternate_environment or change the value of alternate_environment variable in this script (line 9)"
+  exit 1
+fi
+
 production_env_group_id=`find_guid "Production environment"`
-echo "I think the \"Production environment\" group uuid is $production_env_group_id"
+echo "\"Production environment\" group uuid is $production_env_group_id"
 agent_specified_env_group_id=`find_guid "Agent-specified environment"`
-echo "I think the \"Agent-specified environment\" group uuid is $agent_specified_env_group_id"
+echo "\"Agent-specified environment\" group uuid is $agent_specified_env_group_id"
+pemaster_group_id=`find_guid "PE Master"`
 #
 # Download starter content and create an alternate puppet environment in addition to production
 #
@@ -42,13 +64,10 @@ echo 'Copying vRO starter content repo into /etc/puppetlabs/code/environments'
 mkdir -p /etc/puppetlabs/code/environments/$alternate_environment
 rm -rf /etc/puppetlabs/code/environments/$alternate_environment/*
 cp -R * /etc/puppetlabs/code/environments/$alternate_environment
-if [ ! -f /etc/puppetlabs/code/environments/$alternate_environment/Puppetfile ]; then
-  echo "Copy operation failed. Aborting script. Be sure to run 'sh scripts/vra_nc_setup.sh' inside the 'puppet-vro-starter_content' directory"
+if [ ! -f /etc/puppetlabs/code/environments/$alternate_environment/modules/vro_plugin_user/manifests/init.pp ]; then
+  echo "ERROR: Copy operation failed. Aborting script. Be sure to run 'sh scripts/vra_nc_setup.sh' inside the 'puppet-vro-starter_content' directory"
   exit 1
 fi
-# Put a copy in production
-rm -rf /etc/puppetlabs/code/environments/production/
-cp -R /etc/puppetlabs/code/environments/$alternate_environment /etc/puppetlabs/code/environments/production
 #
 # Tell the NC to refresh its cache so that the classes we just installed are available
 #
