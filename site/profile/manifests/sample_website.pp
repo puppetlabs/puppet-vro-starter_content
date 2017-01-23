1fd631301d1b29deaf9cafd0db0334282cfd5908
Abir Majumdar <beersy@gmail.com>
2017-01-23 15:06:24 -0800

    Updating linux value in case statement
    to match what facter actually returns on Linux

--- site/profile/manifests/sample_website.pp
+++ site/profile/manifests/sample_website.pp
@@ -3,7 +3,7 @@ class profile::sample_website {
 
   case $::kernel {
     'windows': { include profile::sample_website::windows }
-    'linux':   { include profile::sample_website::linux   }
+    'Linux':   { include profile::sample_website::linux   }
   }
 
 }
