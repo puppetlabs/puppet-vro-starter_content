8dbeb9e104f6f8bd60871fe19afd8cdecd5b421f
maju6406 <beersy@gmail.com>
2017-02-10 15:27:53 -0800

    Removing unnecessary docs

--- site/role/manifests/linux_base.pp
+++ site/role/manifests/linux_base.pp
@@ -1,8 +1,4 @@
 # This role installs a baseline of packages on Linux machines
-#
-# @example Declaring the class
-#   include role::linux_base
-#
 class role::linux_base {
   include profile::linux_baseline
 }
